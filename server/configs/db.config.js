2729bbca7e0c5a55cda85c1f1008368af4b6102b
Emma Northridge <emmanorthridge1@gmail.com>
2020-12-10 11:43:02 +0000

    :sparkles: connected to Mongo Atlas

--- server/configs/db.config.js
+++ server/configs/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/helping-hand", {
+  .connect(process.env.MONGO_ATLAS_URI || MONGO_LOCAL, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -15,3 +15,4 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to mongo", err);
   });
+
