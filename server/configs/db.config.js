4ca95450e9b7c0144fc3b3e39d3a5a0c73641c12
Emma Northridge <emmanorthridge1@gmail.com>
2020-12-11 10:52:47 +0000

    heroku

--- server/configs/db.config.js
+++ server/configs/db.config.js
@@ -1,31 +1,10 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const { MONGO_ATLAS_URI, MONGO_LOCAL, NODE_ENV } = process.env;
+const { MONGO_ATLAS_URI, MONGO_LOCAL, NODE_ENV } = process.env;
 
-// const connectDb = (mongoUri) =>
-// mongoose
-//   .connect(mongoUri, {
-//     useCreateIndex: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//   })
-//   .then((x) => {
-//     console.log(
-//       `✅✅✅ Connected to Mongo! Database name: "${x.connections[0].name}"`
-//     );
-//   })
-//   .catch((err) => {
-//     console.error("Error connecting to mongo", err);
-//   });
-
-// // Check if development environment is
-// NODE_ENV === "development" ? connectDb(MONGO_LOCAL) : connectDb(MONGO_ATLAS_URI);
-
-const mongoose = require('mongoose');
-
+const connectDb = (mongoUri) =>
 mongoose
-  .connect(process.env.MONGO_ATLAS_URI || MONGO_LOCAL, {
+  .connect(mongoUri, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -37,5 +16,26 @@ mongoose
     );
   })
   .catch((err) => {
-    console.error('Error connecting to mongo', err);
+    console.error("Error connecting to mongo", err);
   });
+
+// Check if development environment is
+NODE_ENV === "development" ? connectDb(MONGO_LOCAL) : connectDb(MONGO_ATLAS_URI);
+
+// const mongoose = require('mongoose');
+
+// mongoose
+//   .connect(process.env.MONGO_ATLAS_URI || MONGO_LOCAL, {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   })
+//   .then((x) => {
+//     console.log(
+//       `✅✅✅ Connected to Mongo! Database name: "${x.connections[0].name}"`
+//     );
+//   })
+//   .catch((err) => {
+//     console.error('Error connecting to mongo', err);
+//   });
